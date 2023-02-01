3792e0bac9a7129a3ca67488d65037b2637fe00f
Alex Jure <alexjuredev@gmail.com>
2023-02-01 19:14:51 +0100

    corrections commentaires

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,4 @@
-// Récuperation d'un paramètre de l'URL qui se trouve être l'orderId
+// Récupération d'un paramètre de l'URL qui se trouve être l'orderId
 let url = window.location.href;
 url = new URL(url);
 let id = url.searchParams.get("orderId")
